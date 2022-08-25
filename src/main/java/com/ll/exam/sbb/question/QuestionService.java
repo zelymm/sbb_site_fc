ba24ea15ba417a2994989ec038fa8b92d69f4389
zelymm <dkdlwkrz@naver.com>
2022-08-25 15:12:06 +0900

    [feat]: show up the list depending on sortCode and also set this not null

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -18,9 +18,13 @@ import java.util.List;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public Page<Question> getList(String kw, int page) {
+    public Page<Question> getList(String kw, int page, String sortCode) {
         List<Sort.Order> sorts = new ArrayList<>();
-        sorts.add(Sort.Order.desc("createDate"));
+
+        switch (sortCode) {
+            case "OLD" -> sorts.add(Sort.Order.asc("id"));
+            default -> sorts.add(Sort.Order.desc("id"));
+        }
 
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지에 10까지 가능
 
