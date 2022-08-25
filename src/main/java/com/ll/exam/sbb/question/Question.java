09389233e26822b9056e418b63520b139ac8c459
zelymm <dkdlwkrz@naver.com>
2022-08-26 02:41:10 +0900

    [feat]: add the Hit count column and expose it on pages

--- src/main/java/com/ll/exam/sbb/question/Question.java
+++ src/main/java/com/ll/exam/sbb/question/Question.java
@@ -35,6 +35,8 @@ public class Question {
     @ManyToMany
     Set<SiteUser> voter;
 
+    private Integer hitCount = 0;
+
     public void addAnswer(Answer answer) {
         answer.setQuestion(this);
         getAnswerList().add(answer);
